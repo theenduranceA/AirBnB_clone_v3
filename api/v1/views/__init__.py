fedee89ffb5192c7ad686ccc2a34f7a3869154fc
theenduranceA <theenduranceaneke@gmail.com>
2023-10-27 08:55:33 -0700

    Updated Blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Blueprint instance."""
+from flask import Blueprint
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from flask import Blueprint
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
