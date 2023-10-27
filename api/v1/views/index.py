d293b54b888b065c05c599cd72add90e0178b19e
theenduranceA <theenduranceaneke@gmail.com>
2023-10-27 10:38:41 -0700

    Updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Returns JSON."""
 
+from flask import Flask
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views, Amenity, City, Place, Review, State, User
