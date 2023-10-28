e33153f2cbf3af344e2040eb61347f1669e236bd
theenduranceA <theenduranceaneke@gmail.com>
2023-10-28 03:38:59 -0700

    Task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """Modules for states."""
 
-from api.v1.views import *
 from flask import Flask, request, jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """ Retrieves the list of all State objects."""
-    states = [state.to_dict() for state in storage.all(State).values()]
-    return jsonify(states)
+    all_states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(all_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -20,7 +18,7 @@ def gets_state(state_id):
     """ Retrieves a State object."""
     state = storage.get(State, state_id)
     if state is None:
-        return jsonify({"error": "Not found"}, 404)
+        abort(404)
     return jsonify(state.to_dict())
 
 
@@ -29,8 +27,8 @@ def gets_state(state_id):
 def deletes_state(state_id):
     """ Deletes a State object."""
     state = storage.get(State, state_id)
-    if state is None:
-        return jsonify({"error": "Not found"}, 404)
+    if _state is None:
+        abort(404)
     storage.delete(state)
     storage.save()
     return jsonify({})
@@ -54,7 +52,7 @@ def updates_state(state_id):
     """ Updates the state object."""
     state = storage.get(State, state_id)
     if state is None:
-        return jsonify({"error": "Not found"}, 404)
+        abort(404)
 
     data = request.get_json()
     if data is None:
