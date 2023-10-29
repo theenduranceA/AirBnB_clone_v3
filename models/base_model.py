f314610d29136dec00947e4a14b7f6cb41adbbda
theenduranceA <theenduranceaneke@gmail.com>
2023-10-29 16:44:34 -0700

    Task 14

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, pass_word=None):
+    def to_dict(self, pass_word=True):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,9 +68,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if pass_word is None:
+        if pass_word:
             if "password" in new_dict:
-                del new_dict['password']
+                del new_dict["password"]
         return new_dict
 
     def delete(self):
