311757aae47453592dc8a96ea722c9cf0edf5654
theenduranceA <theenduranceaneke@gmail.com>
2023-10-29 09:01:53 -0700

    Users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -9,16 +9,15 @@ from models import storage
 @app_views.route('/api/v1/users', methods=['GET'], strict_slashes=False)
 def get_all_users():
     """ Retrieves the list of all User objects."""
-    users = User.query.all()
-    user_list = [user.to_dict() for user in users]
-    return jsonify(user_list)
+    all_users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(all_users)
 
 
 @app_views.route(
         '/api/v1/users/<int:user_id>', methods=['GET'], strict_slashes=False)
 def gets_user(user_id):
     """ Retrieves a User object."""
-    user = User.query.get(user_id)
+    user = storage.get(User, user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_dict())
@@ -29,10 +28,11 @@ def gets_user(user_id):
         strict_slashes=False)
 def deletes_user(user_id):
     """ Deletes a User object."""
-    user = User.query.get(user_id)
+    user = storage.get(User, user_id)
     if user is None:
         abort(404)
     user.delete()
+    storage.save()
     return jsonify({})
 
 
@@ -56,7 +56,7 @@ def creates_user():
         '/api/v1/users/<int:user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """ Updates a User object."""
-    user = User.query.get(user_id)
+    user = storage.get(User, user_id)
     if user is None:
         abort(404)
 
@@ -70,5 +70,5 @@ def update_user(user_id):
     for key, value in data.items():
         setattr(user, key, value)
 
-    user.save()
+    storage.save()
     return jsonify(user.to_dict())
